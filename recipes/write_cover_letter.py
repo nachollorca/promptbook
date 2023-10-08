de30993c596bf7ab1d7e2c99f39ee66b634e0fd0
nachollorca <madwayesp@gmail.com>
2023-10-08 12:42:08 +0200

    Include UI info for cover letters

--- recipes/write_cover_letter.py
+++ recipes/write_cover_letter.py
@@ -1,6 +1,6 @@
 def write_cover_letter(
         job: str,
-        cv: str,
+        resume: str,
         style: str = None,
         vocabulary: str = None,
 ) -> str:
@@ -11,7 +11,7 @@ Your task is to write a cover letter for the job posting delimited by quotation 
 
 Use the information from my resume delimited by quotation marks below, only when you feel it advantageous.
 
-"{cv}"
+"{resume}"
 
 Perform the following actions:
  1. Write a paragraph of maximum 6 sentences explaining why I would like to work with them and praising their business.
@@ -26,3 +26,19 @@ Perform the following actions:
         prompt = prompt + f"Use {vocabulary} vocabulary."
 
     return prompt
+
+
+_title = "Cover Letter Builder"
+_description = "Produce quick Cover Letters by just introducing the job offer and information about yourself."
+_ui = {
+    "resume": {
+        "help": "Your CV. If your resume supports ATS (it really should, check online), you could just copy paste its text in here.",
+    },
+    "style": {
+        "suggestions": "simple, formal, creative and excited..."
+    },
+    "vocabulary": {
+        "suggestions": "technical, academic..."
+    },
+}
+
